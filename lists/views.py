67bddf58908cde5d9966a941e18b138921da556d
maxb <BelkiMa1@PSTW15249.netti.infra.posti.fi>
2015-04-22 15:10:40 +0300

    Expanded functionality that post the inputed value of to-do list to a page.

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    # if request.method == 'POST':
+        # return render(request, 'home.html', {
+            # 'new_item_text': request.POST['item_text'],
+        # })
+    # return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+    })
