2a745d8fae4fd72efcfa04fa78015a365c460cbd
MaxB <bernardito.luis.el.gorra@gmail.com>
2015-04-23 16:44:49 +0300

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='home'),
 
     # url(r'^admin/', include(admin.site.urls)),
 )
