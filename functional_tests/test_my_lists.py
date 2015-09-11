650dc0499fdad65a6056061b6fa0f98fab612e58
MaxB <bernardito.luis.el.gorra@gmail.com>
2015-09-11 17:34:05 +0300

    placeholder test_my_lists and move login checkers into base

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -25,3 +25,14 @@ class MyListsTest(FunctionalTest):
             path='/',
         ))
 
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'elaine@example.com'
+
+        self.browser.get(self.server_url)
+        self.wait_to_be_logged_out(email)
+
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session(email)
+
+        self.browser.get(self.server_url)
+        self.wait_to_be_logged_in(email)
